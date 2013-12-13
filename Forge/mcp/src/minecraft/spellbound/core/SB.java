940f80d542fc3fc378acbaa94b5110be91a9a3a1
WildBamaBoy <dustya@otelco.net>
2013-12-12 23:36:40 -0600

    Finally

---
+++ Forge/mcp/src/minecraft/spellbound/core/SB.java
@@ -0,0 +1,33 @@
+/**********************************************
+ * SB.java
+ * Copyright (c) 2013 MCA Dev Team.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ **********************************************/
+
+package spellbound.core;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+
+@Mod(modid="Spellbound", name="Spellbound", version="1.0.0")
+@NetworkMod(clientSideRequired=true, serverSideRequired=false,
+channels={"CHANNEL"}
+//packethandler = Packethandler.class //*
+)
+public class SB 
+{
+	@Instance("Spellbound")
+	public static SB instance;
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		instance = this;
+	}
+}
---
+++ Forge/mcp/src/minecraft/spellbound/external/PropertiesManager.java
@@ -0,0 +1,15 @@
+/**********************************************
+ * PropertiesManager.java
+ * Copyright (c) 2013 MCA Dev Team.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ **********************************************/
+
+package spellbound.external;
+
+public class PropertiesManager 
+{
+	public PropertiesList
+}
