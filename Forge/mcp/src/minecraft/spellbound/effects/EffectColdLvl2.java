ce441ca58fd1cf990bd7a70c077b71a1b0eacf53
WildBamaBoy <dustya@otelco.net>
2013-12-14 16:38:28 -0600

    a

--- Forge/mcp/src/minecraft/spellbound/effects/EffectColdLvl2.java
+++ Forge/mcp/src/minecraft/spellbound/effects/EffectColdLvl2.java
@@ -2,7 +2,9 @@ package spellbound.effects;
 
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
+import spellbound.entity.EntityTargetSpell;
 import spellbound.enums.EnumSpellType;
 
 public class EffectColdLvl2 extends AbstractEffect
@@ -16,15 +18,29 @@ public class EffectColdLvl2 extends AbstractEffect
 	@Override
 	public void doSpellEffect(EntityPlayer caster) 
 	{
-		
+		if (!caster.worldObj.isRemote)
+		{
+			caster.inventory.consumeInventoryItem(caster.inventory.currentItem);
+			caster.worldObj.playSoundAtEntity(caster, "mob.ghast.fireball", 1.0F, 1.0F);
+
+			Vec3 vec = caster.getLookVec();
+			EntityTargetSpell spell = new EntityTargetSpell(caster.worldObj, this);
+			spell.setPosition(caster.posX + vec.xCoord * 5, caster.posY + 1 + vec.yCoord * 5, caster.posZ + vec.zCoord * 5);
+
+			spell.accelerationX = vec.xCoord * 0.3;
+			spell.accelerationY = vec.yCoord * 0.3;
+			spell.accelerationZ = vec.zCoord * 0.3;
+
+			caster.worldObj.spawnEntityInWorld(spell);
+		}
 	}
 
 	@Override
 	public void updateSpellEffect() 
 	{
-		
+
 	}
-	
+
 	@Override
 	public EnumSpellType getSpellType() 
 	{
@@ -34,6 +50,6 @@ public class EffectColdLvl2 extends AbstractEffect
 	@Override
 	public void doSpellTargetEffect(World worldObj, int posX, int posY, int posZ, EntityLivingBase entityHit) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
