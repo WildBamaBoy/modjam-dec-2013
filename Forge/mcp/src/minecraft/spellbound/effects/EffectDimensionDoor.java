8d4681558d668ff725542ce92b69676f5b877f62
WildBamaBoy <dustya@otelco.net>
2013-12-14 12:50:19 -0600

    wall spells

--- Forge/mcp/src/minecraft/spellbound/effects/EffectDimensionDoor.java
+++ Forge/mcp/src/minecraft/spellbound/effects/EffectDimensionDoor.java
@@ -20,63 +20,60 @@ public class EffectDimensionDoor extends AbstractEffect
 	@Override
 	public void doSpellEffect(EntityPlayer caster) 
 	{
-		if (!caster.worldObj.isRemote)
+		caster.worldObj.playSoundAtEntity(caster, "mob.endermen.portal", 1.0F, 1.0F);
+
+		int heading = MathHelper.floor_double((double)(caster.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
+		int transportAmount = SB.rand.nextInt(50) + 50;
+
+		double newPlayerX = 0.0D;
+		double newPlayerY = 0.0D;
+		double newPlayerZ = 0.0D;
+
+		if (heading == 0)
 		{
-			caster.worldObj.playSoundAtEntity(caster, "mob.endermen.portal", 1.0F, 1.0F);
-			
-			int heading = MathHelper.floor_double((double)(caster.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
-			int transportAmount = SB.rand.nextInt(50) + 50;
-			
-			double newPlayerX = 0.0D;
-			double newPlayerY = 0.0D;
-			double newPlayerZ = 0.0D;
-			
-			if (heading == 0)
-			{
-				newPlayerX = caster.posX;
-				newPlayerY = 255;
-				newPlayerZ = caster.posZ + transportAmount;
-			}
-			
-			else if (heading == 1)
-			{
-				newPlayerX = caster.posX - transportAmount;
-				newPlayerY = 255;
-				newPlayerZ = caster.posZ;
-			}
-			
-			else if (heading == 2)
-			{
-				newPlayerX = caster.posX;
-				newPlayerY = 255;
-				newPlayerZ = caster.posZ - transportAmount;
-			}
-			
-			else if (heading == 3)
-			{
-				newPlayerX = caster.posX + transportAmount;
-				newPlayerY = 255;
-				newPlayerZ = caster.posZ;
-			}
-			
-			while (caster.worldObj.getBlockId((int)newPlayerX, (int)newPlayerY, (int)newPlayerZ) == 0 && newPlayerY != 0)
-			{
-				newPlayerY--;
-			}
-			
-			EntityPlayerMP casterMP = (EntityPlayerMP)caster;
-			casterMP.mountEntity((Entity)null);
-			casterMP.playerNetServerHandler.setPlayerLocation(newPlayerX, newPlayerY, newPlayerZ, caster.rotationYaw, caster.rotationPitch);
-			caster.worldObj.playSoundAtEntity(casterMP, "mob.endermen.portal", 1.0F, 1.0F);
+			newPlayerX = caster.posX;
+			newPlayerY = 255;
+			newPlayerZ = caster.posZ + transportAmount;
 		}
+
+		else if (heading == 1)
+		{
+			newPlayerX = caster.posX - transportAmount;
+			newPlayerY = 255;
+			newPlayerZ = caster.posZ;
+		}
+
+		else if (heading == 2)
+		{
+			newPlayerX = caster.posX;
+			newPlayerY = 255;
+			newPlayerZ = caster.posZ - transportAmount;
+		}
+
+		else if (heading == 3)
+		{
+			newPlayerX = caster.posX + transportAmount;
+			newPlayerY = 255;
+			newPlayerZ = caster.posZ;
+		}
+
+		while (caster.worldObj.getBlockId((int)newPlayerX, (int)newPlayerY, (int)newPlayerZ) == 0 && newPlayerY != 0)
+		{
+			newPlayerY--;
+		}
+
+		EntityPlayerMP casterMP = (EntityPlayerMP)caster;
+		casterMP.mountEntity((Entity)null);
+		casterMP.playerNetServerHandler.setPlayerLocation(newPlayerX, newPlayerY, newPlayerZ, caster.rotationYaw, caster.rotationPitch);
+		caster.worldObj.playSoundAtEntity(casterMP, "mob.endermen.portal", 1.0F, 1.0F);
 	}
 
 	@Override
 	public void updateSpellEffect() 
 	{
-		
+
 	}
-	
+
 	@Override
 	public EnumSpellType getSpellType() 
 	{
--- Forge/mcp/src/minecraft/spellbound/effects/EffectWallOfBedrock.java
+++ Forge/mcp/src/minecraft/spellbound/effects/EffectWallOfBedrock.java
@@ -2,6 +2,7 @@ package spellbound.effects;
 
 import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 import spellbound.enums.EnumSpellType;
 
@@ -16,7 +17,67 @@ public class EffectWallOfBedrock extends AbstractEffectWall
 	@Override
 	public void doSpellEffect(EntityPlayer caster) 
 	{
+		caster.worldObj.playSoundAtEntity(caster, "mob.endermen.portal", 1.0F, 1.0F);
+
+		int heading = MathHelper.floor_double((double)(caster.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
+
+		int wallCenterX = 0;
+		int wallCenterY = 0;
+		int wallCenterZ = 0;
+
+		if (heading == 0)
+		{
+			wallCenterX = (int) caster.posX;
+			wallCenterY = (int) caster.posY;
+			wallCenterZ = (int) (caster.posZ + 3);
+		}
+
+		else if (heading == 1)
+		{
+			wallCenterX = (int) (caster.posX - 3);
+			wallCenterY = (int) caster.posY;
+			wallCenterZ = (int) caster.posZ;
+		}
+
+		else if (heading == 2)
+		{
+			wallCenterX = (int) caster.posX;
+			wallCenterY = (int) caster.posY;
+			wallCenterZ = (int) (caster.posZ - 3);
+		}
+
+		else if (heading == 3)
+		{
+			wallCenterX = (int) (caster.posX + 3);
+			wallCenterY = (int) caster.posY;
+			wallCenterZ = (int) caster.posZ;
+		}
 		
+		for (int i = -3; i < 4; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				if (heading == 0)
+				{
+					caster.worldObj.setBlock(wallCenterX + i, wallCenterY + j, wallCenterZ, getWallBlockId());
+				}
+
+				else if (heading == 1)
+				{
+					caster.worldObj.setBlock(wallCenterX, wallCenterY + j, wallCenterZ + i, getWallBlockId());
+				}
+
+				else if (heading == 2)
+				{
+					caster.worldObj.setBlock(wallCenterX + i, wallCenterY + j, wallCenterZ, getWallBlockId());
+				}
+
+				else if (heading == 3)
+				{
+					caster.worldObj.setBlock(wallCenterX, wallCenterY + j, wallCenterZ - i, getWallBlockId());
+				}
+			}
+		}
 	}
 
 	@Override
