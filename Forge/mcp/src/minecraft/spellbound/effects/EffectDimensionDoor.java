5603462db0eef9cf92e2765e6123e66b2fd9aa1c
WildBamaBoy <dustya@otelco.net>
2013-12-14 15:56:36 -0600

    a

--- Forge/mcp/src/minecraft/spellbound/effects/EffectDimensionDoor.java
+++ Forge/mcp/src/minecraft/spellbound/effects/EffectDimensionDoor.java
@@ -22,50 +22,53 @@ public class EffectDimensionDoor extends AbstractEffect
 	{
 		caster.worldObj.playSoundAtEntity(caster, "mob.endermen.portal", 1.0F, 1.0F);
 
-		int heading = MathHelper.floor_double((double)(caster.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
-		int transportAmount = SB.rand.nextInt(50) + 50;
-
-		double newPlayerX = 0.0D;
-		double newPlayerY = 0.0D;
-		double newPlayerZ = 0.0D;
-
-		if (heading == 0)
+		if (!caster.worldObj.isRemote)
 		{
-			newPlayerX = caster.posX;
-			newPlayerY = 255;
-			newPlayerZ = caster.posZ + transportAmount;
-		}
+			int heading = MathHelper.floor_double((double)(caster.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
+			int transportAmount = SB.rand.nextInt(50) + 50;
 
-		else if (heading == 1)
-		{
-			newPlayerX = caster.posX - transportAmount;
-			newPlayerY = 255;
-			newPlayerZ = caster.posZ;
-		}
+			double newPlayerX = 0.0D;
+			double newPlayerY = 0.0D;
+			double newPlayerZ = 0.0D;
 
-		else if (heading == 2)
-		{
-			newPlayerX = caster.posX;
-			newPlayerY = 255;
-			newPlayerZ = caster.posZ - transportAmount;
-		}
+			if (heading == 0)
+			{
+				newPlayerX = caster.posX;
+				newPlayerY = 255;
+				newPlayerZ = caster.posZ + transportAmount;
+			}
 
-		else if (heading == 3)
-		{
-			newPlayerX = caster.posX + transportAmount;
-			newPlayerY = 255;
-			newPlayerZ = caster.posZ;
-		}
+			else if (heading == 1)
+			{
+				newPlayerX = caster.posX - transportAmount;
+				newPlayerY = 255;
+				newPlayerZ = caster.posZ;
+			}
 
-		while (caster.worldObj.getBlockId((int)newPlayerX, (int)newPlayerY, (int)newPlayerZ) == 0 && newPlayerY != 0)
-		{
-			newPlayerY--;
-		}
+			else if (heading == 2)
+			{
+				newPlayerX = caster.posX;
+				newPlayerY = 255;
+				newPlayerZ = caster.posZ - transportAmount;
+			}
 
-		EntityPlayerMP casterMP = (EntityPlayerMP)caster;
-		casterMP.mountEntity((Entity)null);
-		casterMP.playerNetServerHandler.setPlayerLocation(newPlayerX, newPlayerY, newPlayerZ, caster.rotationYaw, caster.rotationPitch);
-		caster.worldObj.playSoundAtEntity(casterMP, "mob.endermen.portal", 1.0F, 1.0F);
+			else if (heading == 3)
+			{
+				newPlayerX = caster.posX + transportAmount;
+				newPlayerY = 255;
+				newPlayerZ = caster.posZ;
+			}
+
+			while (caster.worldObj.getBlockId((int)newPlayerX, (int)newPlayerY, (int)newPlayerZ) == 0 && newPlayerY != 0)
+			{
+				newPlayerY--;
+			}
+
+			EntityPlayerMP casterMP = (EntityPlayerMP)caster;
+			casterMP.mountEntity((Entity)null);
+			casterMP.playerNetServerHandler.setPlayerLocation(newPlayerX, newPlayerY, newPlayerZ, caster.rotationYaw, caster.rotationPitch);
+			caster.worldObj.playSoundAtEntity(casterMP, "mob.endermen.portal", 1.0F, 1.0F);
+		}
 	}
 
 	@Override
--- Forge/mcp/src/minecraft/spellbound/effects/EffectUltimateElementalFury.java
+++ Forge/mcp/src/minecraft/spellbound/effects/EffectUltimateElementalFury.java
@@ -4,10 +4,15 @@ import java.util.List;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.effect.EntityLightningBolt;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
+import spellbound.core.SB;
 import spellbound.entity.EntityTargetSpell;
 import spellbound.enums.EnumSpellType;
 
@@ -54,19 +59,43 @@ public class EffectUltimateElementalFury extends AbstractEffect
 	@Override
 	public void doSpellTargetEffect(World worldObj, int posX, int posY, int posZ, EntityLivingBase entityHit) 
 	{
-		int radius = 3;
+		if (!worldObj.isRemote)
+		{
+		int radius = 8;
 
-//		List entitiesList = entityHit == null ? 
-//				worldObj.getEntitiesWithinAABB(Entity.class, AxisAlignedBB.getBoundingBox(posX - radius, posY - radius, posZ - radius, posX + radius, posY + radius, posZ + radius)) : 
-//					worldObj.getEntitiesWithinAABB(AxisAlignedBB.getBoundingBox(entityHit.posX - radius, entityHit.posY - radius, entityHit.posZ - radius, entityHit.posX + radius, entityHit.posY + radius, entityHit.posZ + radius));
-//
-//				for (Object obj : entitiesList)
-//				{
-//					if (obj instanceof EntityLivingBase)
-//					{
-//						EntityLivingBase livingEntity = (EntityLivingBase)obj;
-//						System.out.println(obj);
-//					}
-//				}
+		List entitiesList = entityHit == null ? 
+				worldObj.getEntitiesWithinAABB(Entity.class, AxisAlignedBB.getBoundingBox(posX - radius, posY - radius, posZ - radius, posX + radius, posY + radius, posZ + radius)) : 
+					worldObj.getEntitiesWithinAABB(Entity.class, AxisAlignedBB.getBoundingBox(entityHit.posX - radius, entityHit.posY - radius, entityHit.posZ - radius, entityHit.posX + radius, entityHit.posY + radius, entityHit.posZ + radius));
+
+				for (Object obj : entitiesList)
+				{
+					if (obj instanceof EntityLivingBase)
+					{
+						EntityLivingBase livingEntity = (EntityLivingBase)obj;
+						
+						if (SB.rand.nextBoolean())
+						{
+							//Cold
+							livingEntity.attackEntityFrom(DamageSource.magic, 15.0F);
+							livingEntity.addPotionEffect(new PotionEffect(Potion.moveSlowdown.id, 1200));
+						}
+						
+						else if (SB.rand.nextBoolean())
+						{
+							livingEntity.attackEntityFrom(DamageSource.magic, 10.0F);
+							livingEntity.setFire(15);
+							worldObj.createExplosion(livingEntity, livingEntity.posX, livingEntity.posY, livingEntity.posZ, 5.0F, false);
+						}
+						
+						else
+						{
+							livingEntity.attackEntityFrom(DamageSource.magic, 15.0F);
+							EntityLightningBolt lightning = new EntityLightningBolt(worldObj, posX, posY, posZ);
+							lightning.setPosition(posX, posY, posZ);
+							worldObj.spawnEntityInWorld(lightning);
+						}
+					}
+				}
+		}
 	}
 }
