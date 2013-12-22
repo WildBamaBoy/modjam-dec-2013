a5f3837ca1eae668b97525fa7f5a845b452874cb
WildBamaBoy <dustya@otelco.net>
2013-12-22 00:48:06 -0600

    Advance Time makes trees grow

--- Forge/mcp/src/minecraft/spellbound/spells/SpellAdvanceTime.java
+++ Forge/mcp/src/minecraft/spellbound/spells/SpellAdvanceTime.java
@@ -9,8 +9,13 @@
 
 package spellbound.spells;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockSapling;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemDye;
+import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import spellbound.core.Constants;
 import spellbound.core.SpellboundCore;
@@ -41,6 +46,16 @@ public class SpellAdvanceTime extends AbstractSpell
 			{
 				caster.worldObj.setBlockMetadataWithNotify(point.posX, point.posY, point.posZ, 7, 2);
 			}
+		}
+
+		for (final Point3D point : Logic.getNearbyBlocks(caster.worldObj, (int)caster.posX, (int)caster.posY, (int)caster.posZ, 25, Block.sapling.blockID))
+		{
+			ItemDye.applyBonemeal(new ItemStack(Item.dyePowder, 1, 13), caster.worldObj, point.posX, point.posY, point.posZ, caster);
+			ItemDye.applyBonemeal(new ItemStack(Item.dyePowder, 1, 13), caster.worldObj, point.posX, point.posY, point.posZ, caster);
+			ItemDye.applyBonemeal(new ItemStack(Item.dyePowder, 1, 13), caster.worldObj, point.posX, point.posY, point.posZ, caster);
+			ItemDye.applyBonemeal(new ItemStack(Item.dyePowder, 1, 13), caster.worldObj, point.posX, point.posY, point.posZ, caster);
+			ItemDye.applyBonemeal(new ItemStack(Item.dyePowder, 1, 13), caster.worldObj, point.posX, point.posY, point.posZ, caster);
+			ItemDye.applyBonemeal(new ItemStack(Item.dyePowder, 1, 13), caster.worldObj, point.posX, point.posY, point.posZ, caster);
 		}
 	}
 
